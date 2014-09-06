325dbe10378d8738f06d5537b320b73aabed4868
m4dz <code@m4dz.net>
2014-09-06 09:35:14 +0200

    Read DM and restitute them properly

--- leon4twitter.py
+++ leon4twitter.py
@@ -10,4 +10,20 @@ api = twitter.Api(consumer_key = config.get('oauth', 'consumer_key'),
                   access_token_key = config.get('oauth', 'access_token_key'),
                   access_token_secret = config.get('oauth', 'access_token_secret'))
 
-print api.VerifyCredentials()
+logs = dict()
+
+for dm in api.GetDirectMessages():
+    if not dm.sender_id in logs:
+        logs[dm.sender_id] = list()
+    logs[dm.sender_id].append(dm)
+
+for dm in api.GetSentDirectMessages():
+    if not dm.recipient_id in logs:
+        logs[dm.recipient_id] = list()
+    logs[dm.recipient_id].append(dm)
+
+for id, l in logs.iteritems():
+    user = api.GetUser(id)
+    print user.screen_name
+    for dm in sorted(l, key=lambda dm: dm.created_at):
+        print "(%s) %s" % (dm.sender_screen_name, dm.text)
